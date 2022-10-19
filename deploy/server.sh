4cb90a4cd18eb6b7a93a2c13114fd3021c3d123f
twmiller <twmiller@iastate.edu>
2022-10-18 20:49:58 -0500

    test

--- deploy/server.sh
+++ deploy/server.sh
@@ -1,14 +1,8 @@
 # Pull code
-pwd
-hostnamectl
-curl -s https://deb.nodesource.com/setup_16.x | sudo -S bash
-sudo -S apt install nodejs -y
-node -v
 cd ./sdmay23-40/server
 git checkout master
 git pull origin master
 
 # Build and deploy
-node -v
 npm install --production
 node server.js
