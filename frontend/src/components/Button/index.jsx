69dd646552a0acf9ad840bd04f66ade49889e23a
bburt8701 <bburt8701@gmail.com>
2022-10-24 19:55:00 -0500

    Issue 10: Pulled in master, moved files.

--- frontend/src/components/Button/index.jsx
+++ frontend/src/components/Button/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./button.css";
+import "../../routes/Root/styles.css";
 
 const STYLES = [
     "btn--post--solid",
--- frontend/src/routes/Root/index.jsx
+++ frontend/src/routes/Root/index.jsx
@@ -5,13 +5,26 @@
  */
 
  import React from "react";
+ import Button from "../../components/Button/index";
 
  function Root(props) {
 	
 	return (
 		<div>
 			<h1>Title Text</h1>
+			<Button
+    			onClick={() => {
+       				 console.log("This can be any function!");
+    			}}
+   				type="button"
+    			buttonStyle="btn--post--outline"
+    			buttonSize="btn--large"
+    		>
+			POST
+   			 </Button>
 		</div>
+
+	
 	);
 
  }
