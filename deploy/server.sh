b970677eaa59e88aa5aff9aec1942e9f0e303566
twmiller <twmiller@iastate.edu>
2022-11-10 17:58:33 -0600

    adding front-end to deploy

--- deploy/server.sh
+++ deploy/server.sh
@@ -3,11 +3,12 @@ cd ./sdmay23-40/frontend
 
 git checkout master
 git pull origin master
-
-npm install --omit=dev
+ls
+npm ci --omit=dev
 npm run build
 
 cd ../server
+ls
 # Build and deploy
-npm install --omit=dev
+npm ci --omit=dev
 /usr/bin/pm2 restart server.js
