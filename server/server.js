423643ddd5e99ff61ec6db757096fccd3011f8ef
Tyler Miller <miller.tyler094@gmail.com>
2022-11-15 08:46:09 -0600

    switching to https

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ const express = require('express')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+const https = require("https");
 const sessions = require('express-session');
 const cookieParser = require('cookie-parser');
 const app = express();
 const routes = require('./api/routes');
 const websockets = require('./websockets/websockets');
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 443;
 const path = require('path');
 const fs = require('fs');
 
@@ -17,7 +18,7 @@ app.use(express.static(__dirname));
 app.use(cookieParser());
 
 
-  app.use(sessions({
+ app.use(sessions({
   secret: process.env.SESSION_SECRET,
   saveUninitialized: true,
   cookie: { maxAge: 86400000 },
@@ -55,9 +56,17 @@ app.get('/*', function(req, res) {
 const webSocketServer = new (require('ws')).Server({ noServer: true });
 webSocketServer.on('connection', websockets.handleRequest)
 
-const server = app.listen(port, () => {
-  console.log(`Server Listening on Port ${port}`);
-});
+const server = https
+  .createServer(
+    {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cert"),
+    },
+    app
+  )
+  .listen(3000, function () {
+    console.log(`Server Listening on Port ${port}`);
+  });
 
 server.on('upgrade', (request, socket, head) => {
   webSocketServer.handleUpgrade(request, socket, head, socket => {
