86e61ee0b611bfd347b348da072d83c6a0e07f45
aswen2000 <aswen2000@gmail.com>
2023-01-26 15:45:38 -0600

    maybe insert into media metadata

--- server/api/media/uploadMedia.js
+++ server/api/media/uploadMedia.js
@@ -1,14 +1,19 @@
+const { v4 } = require('uuid');
+
 /**
  * @param {*} req 
  * req.body = {
  *    course_id: int,
- *    session_id: int
+ *    session_id: int,
+ *    message_id: int,
+ *    media_id: String
  * }
  * @param {*} res 
  * @returns guid of uploaded media
  */
 const uploadMedia = async (req, res) => {
     console.log("img", req.body);
+
     return res.status(200).send({ guid: 'guid goes here' });
 }
 
--- server/api/message/message.js
+++ server/api/message/message.js
@@ -1,4 +1,19 @@
+const { v4 } = require('uuid');
+
 const { runQuery } = require('../../utils/db_connection');
+
+const msgQuery = `INSERT INTO messages (
+    sender_id, 
+    lecture_id, 
+    timestamp, 
+    is_anonymous, 
+    body,
+    parent_id) VALUES
+    (
+        ?, ?, NOW(), ?, ?, ?
+    );`;
+
+const mediaQuery = `INSERT INTO media_metadata ( media_id, course_id, message_id, timestamp ) VALUES ( ?, ?, ?, NOW());`;
 
 /**
  * 
@@ -6,38 +21,49 @@ const { runQuery } = require('../../utils/db_connection');
  * req.body = {
  *    sender_id: int,
  *    body: string,
- *    lecture_id: int
+ *    lecture_id: int,
+ *    course_id: int, (REQUIRED FOR MEDIA)
  *    is_anonymous: bool
  *    parent_id: <optional>int
  *    session_id: int
+ *    has_media: bool
  * }
  * @param {*} res 
  * @returns message_id of created message
  */
 const addMessage = async (req, res) => {
     try {
-        const { sender_id: senderId, body, is_anonymous: isAnonymous, lecture_id: lectureId, parent_id: parentID } = req.body;
-        if (!senderId || !body || !isAnonymous || !lectureId) {
+        const { sender_id: senderId, body, is_anonymous: isAnonymous, lecture_id: lectureId, course_id: courseId, parent_id: parentID, has_media: hasMedia = false } = req.body;
+
+        console.log(senderId);
+        console.log(body);
+        console.log(isAnonymous);
+        console.log(lectureId);
+
+        if (!senderId || !body || !(typeof isAnonymous === "boolean") || !lectureId) {
             return res.status(400).send({ msg: "Invalid Body" })
         }
-        const query = `INSERT INTO messages (
-            sender_id, 
-            lecture_id, 
-            timestamp, 
-            is_anonymous, 
-            body,
-            parent_id) VALUES
-            (
-                ?, ?, NOW(), ?, ?, ?
-            );`
-        const resp = await runQuery(query, [
+
+        const msgQueryResp = await runQuery(msgQuery, [
             senderId,
             lectureId,
             isAnonymous,
             body,
             parentID ?? null,
         ]);
-        return res.status(201).send({ message_id: resp.insertId })
+
+        let mediaQueryResp;
+        const mediaGUID = v4();
+        if (hasMedia) {
+            mediaQueryResp = await runQuery(mediaQuery, [
+                mediaGUID,
+                courseId,
+                msgQueryResp.insertId
+            ]);
+        }
+
+        return res.status(201).send({ message_id: msgQueryResp.insertId, media_id: mediaQueryResp.insertId });
+
     } catch (e){
         console.error(e);
         return res.status(500).send({ msg: 'Internal Server Error' })
