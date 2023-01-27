bb51fea7d16c81cdfab5f8a3fc683cc7cbea9ffd
aswen2000 <aswen2000@gmail.com>
2023-01-27 16:24:09 -0600

    save file to fs?

--- server/api/media/uploadMedia.js
+++ server/api/media/uploadMedia.js
@@ -1,10 +1,9 @@
-const { v4 } = require('uuid');
+const fs = require('fs');
 
 /**
  * @param {*} req 
  * req.body = {
  *    course_id: int,
- *    session_id: int,
  *    message_id: int,
  *    media_id: String
  * }
@@ -13,6 +12,31 @@ const { v4 } = require('uuid');
  */
 const uploadMedia = async (req, res) => {
     console.log("img", req.body);
+
+    const {
+        course_id: courseID,
+        message_id: messageID,
+        media_id: mediaID,
+    } = req.body;
+    const dir = `../../../../sdmay23-40_media/${courseID}`;
+
+    // TODO: add guid check from metadata here
+
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true }); 
+    }
+
+    try {
+        fs.writeFile(`${mediaID}.jpeg`, req.body, (error) => { //not sure how handle file extension yet
+          if (error) {
+            throw error;
+          }
+        });
+  
+        res.sendStatus(200);
+      } catch (error) {
+        res.sendStatus(500);
+      }
 
     return res.status(200).send({ guid: 'guid goes here' });
 }
