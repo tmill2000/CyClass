6e9d247b45f64b0206234ee74fd4091391be1fbd
aswen2000 <aswen2000@gmail.com>
2023-01-27 18:59:31 -0600

    wrap in try/catch

--- server/api/media/uploadMedia.js
+++ server/api/media/uploadMedia.js
@@ -11,33 +11,39 @@ const mediaService = require('./services/mediaService');
  * @returns guid of uploaded media
  */
 const uploadMedia = async (req, res) => { // TODO: wrap in try/catch
-    const {
-        media_id: mediaID,
-    } = req.query;
-
-    if (!mediaID) {
-        return res.status(400).send({ msg: "Invalid Body" });
-    }
-
-    const { course_id: courseID, user_id: userID } = mediaService.authUpload(mediaID);
-
-    if (userID !== req.session.userid) {
-        return res.status(403).send({ msg: "Forbidden to upload file to this mediaID" });
-    }
-
-    const dir = `../../sdmay23-40_media/${courseID}`;
-
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true }); 
-    }
-
-    fs.writeFile(`${dir}/${mediaID}.png`, req.body, (error) => { //not sure how handle file extension yet
-        if (error) {
-            throw error;
+    try {
+        const {
+            media_id: mediaID,
+        } = req.query;
+    
+        if (!mediaID) {
+            return res.status(400).send({ msg: "Invalid Body" });
         }
-    });
-
-    return res.status(200).send({ guid: 'guid goes here' });
+    
+        const { course_id: courseID, user_id: userID } = mediaService.authUpload(mediaID);
+    
+        if (userID !== req.session.userid) {
+            return res.status(403).send({ msg: "Forbidden to upload file to this mediaID" });
+        }
+    
+        const dir = `../../sdmay23-40_media/${courseID}`;
+    
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true }); 
+        }
+    
+        fs.writeFile(`${dir}/${mediaID}.png`, req.body, (error) => { //not sure how handle file extension yet
+            if (error) {
+                throw error;
+            }
+        });
+    
+        return res.status(200).send({ guid: 'guid goes here' });
+    } catch (e) {
+        console.error(e);
+        return res.status(500).send({ msg: 'Internal Server Error' });
+    }
+    
 }
 
 module.exports = {
