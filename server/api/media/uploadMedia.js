660c32c612854826fcb45861622d1060a0e0c315
aswen2000 <aswen2000@gmail.com>
2023-01-29 19:37:47 -0600

    include courseID

--- server/api/media/uploadMedia.js
+++ server/api/media/uploadMedia.js
@@ -15,35 +15,35 @@ const uploadMedia = async (req, res) => { //TODO: Add open-api spec
         const {
             media_id: mediaID,
         } = req.query;
-    
+
         if (!mediaID) {
             return res.status(400).send({ msg: "Invalid Body" });
         }
-    
+
         const { course_id: courseID, user_id: userID } = await mediaService.authUpload(mediaID);
-    
+
         if (userID !== req.session.userid) {
-            return res.status(403).send({ msg: "Forbidden to upload file to this mediaID", usersUserID: userID, sessionID: req.session.userid });
+            return res.status(403).send({ msg: "Forbidden to upload file to this mediaID", usersUserID: userID, sessionID: req.session.userid, courseID });
         }
-    
+
         const dir = `../../sdmay23-40_media/${courseID}`;
-    
+
         if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir, { recursive: true }); 
         }
-    
+
         fs.writeFile(`${dir}/${mediaID}.png`, req.body, (error) => { //not sure how handle file extension yet
             if (error) {
                 throw error;
             }
         });
-    
+
         return res.status(200).send({ guid: 'guid goes here' });
     } catch (e) {
         console.error(e);
         return res.status(500).send({ msg: 'Internal Server Error' });
     }
-    
+
 }
 
 module.exports = {
