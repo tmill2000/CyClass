b6ba6973d420495f49e243d9c04bb4362ba5d4e3
aswen2000 <aswen2000@gmail.com>
2023-01-29 20:24:15 -0600

    add received field to media metadata

--- sql/init.sql
+++ sql/init.sql
@@ -73,6 +73,7 @@ CREATE TABLE media_metadata (
     course_id int NOT NULL,
     message_id int NOT NULL,
     user_id int NOT NULL,
+    received boolean NOT NULL,
     timestamp datetime NOT NULL
 );
 
