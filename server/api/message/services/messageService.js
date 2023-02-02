ef21d58571c57e0c709bddd536f12007f7d32b50
Tyler Miller <miller.tyler094@gmail.com>
2023-02-02 10:35:34 -0600

    close_date is now in ms

--- server/api/message/services/messageService.js
+++ server/api/message/services/messageService.js
@@ -84,7 +84,7 @@ const getMessagesAndPollsByLectureId = async (lectureId, timestamp) => {
                 poll_id: key,
                 timestamp: new Date(value[0].timestamp).getTime(),
                 question: value[0].question_text,
-                close_date: value[0].close_date,
+                close_date: value[0].close_date ? new Date(value[0].close_date).getTime() : null,
                 closed: !value[0].is_open,
                 choices: value.map((item) => ({
                     poll_choice_id: item.poll_choice_id,
