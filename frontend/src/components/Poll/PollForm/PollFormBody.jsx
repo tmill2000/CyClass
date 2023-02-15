b581d94a03962c3baedb17e9bb0892d1f0f22f82
JadenC1126 <jadenc@iastate.edu>
2023-02-15 17:03:16 -0500

    adding stylet to the popup

--- frontend/src/components/Poll/PollForm/PollFormBody.jsx
+++ frontend/src/components/Poll/PollForm/PollFormBody.jsx
@@ -10,6 +10,21 @@ function PollFormBody(){
                     <InputField></InputField>
                 </div>
             </div>
+            <div className="poll-form-answer-entry-group">
+                <div className="poll-form-answer-label">Enter in your answer choices: </div>
+                <div className="poll-form-answer-group">
+                    <text className="poll-form-individual-answer-label"> A </text>
+                    <input className="poll-form-individual-answer-entry"></input>
+                    <text className="poll-form-individual-answer-label"> B </text>
+                    <input className="poll-form-individual-answer-entry"></input>
+                </div>
+                <div className="poll-form-answer-group">
+                    <text className="poll-form-individual-answer-label"> C </text>
+                    <input className="poll-form-individual-answer-entry"></input>
+                    <text className="poll-form-individual-answer-label"> D </text>
+                    <input className="poll-form-individual-answer-entry"></input>
+                </div>
+            </div>
         </div>
     );
 }
