0bad9b7e1f96a525384d120a0167c60079ce2c08
aswen2000 <aswen2000@gmail.com>
2023-02-20 17:06:16 -0600

    don't allow freeform newValsObj key insertion

--- server/api/utils/patchService.js
+++ server/api/utils/patchService.js
@@ -2,25 +2,23 @@ const { runQuery } = require('../../utils/db_connection');
 
 /**
  * Service to update data objects
- * @param {*} tableName Name of table
+ * @param {*} tableName Name of table, should be hard-coded in calling backend function to avoid SQL injection
  * @param {*} newValsObj Object with modified column names as keys and desired new values as values
- * @param {*} whereClauseCol Column name to based WHERE statement off of
+ * @param {*} whereClauseCol Column name to based WHERE statement off of, should be hard-coded in calling backend function to avoid SQL injection
  * @param {*} whereClauseVal Value to base WHERE statement on
  */
 const genericPatch = async (tableName, newValsObj, whereClauseCol, whereClauseVal) => {
-    const updatesToMake = [], newVals = [];
+    const updatesToMake = [], insertValues = [];
     try {
         Object.keys(newValsObj).forEach((objKey) => {
-
-            updatesToMake.push(`${objKey} = ?`);
-            newVals.push(fields2Chg[objKey]);
-            
+            updatesToMake.push(`? = ?`);
+            insertValues.push(objKey, newValsObj[objKey]);
         });
+
         const joinedUpdates = updatesToMake.join(", ");
         const query = `UPDATE ${tableName} SET ${joinedUpdates} WHERE ${whereClauseCol} = ?`;
 
-        const resp = await runQuery(query, [...newVals, whereClauseVal]);
-
+        const resp = await runQuery(query, [...insertValues, whereClauseVal]);
         return resp;
 
     } catch (e) {
