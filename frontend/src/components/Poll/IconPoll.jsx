d4b51a57d059f4df3194f0bacdcea35cf381cdd6
JadenC1126 <jadenc@iastate.edu>
2023-02-22 15:36:20 -0500

    updating popup style

--- frontend/src/components/Poll/IconPoll.jsx
+++ frontend/src/components/Poll/IconPoll.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import {Circle} from 'react-shapes';
 
 import './style.css';
 
 function IconPoll(){
     return(
         <div className='poll-icon-container'>
-            <Circle r={7} fill={{color: 'rgb(137, 135, 135)'}} stroke={{color:'black'}} strokeWidth={1} />
+            <text className='poll-icon-circle'></text>
             <div className='poll-icon-label'>Poll</div>
         </div>
     );
