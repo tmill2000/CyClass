314d0b03df9e886ec6ceddb5c300eb70ee4f3d4e
bburt8701 <bburt8701@gmail.com>
2023-02-22 14:16:12 -0600

    Changes to poll participation

--- frontend/src/routes/Polls/ParticipantPill.jsx
+++ frontend/src/routes/Polls/ParticipantPill.jsx
@@ -6,15 +6,26 @@
  import React from 'react';
  import "./styles.css";
 
+ const pollResults = (props) => [
+    {
+        user: {
+            id: number,
+            name: string,
+            role: string
+        },
+        option: string,
+        correct: boolean
+    },
+]
 
- function ParticipantPill (props) {
+ const ParticipantPill = (props) => {
 
 	// Component
 	return (
 		<div style={{paddingTop: '50px', paddingLeft: '30px'}}>
     	 	<div style={{borderStyle: 'groove', backgroundColor: '#ededed', width: '510px', height: '60px'}}>
           		<div style={{float: 'right', paddingTop: '20px', paddingRight: '58px', fontFamily: 'Arial', fontWeight: 'bold', fontSize: '18', color: '#32a852'}}>Correct - A</div>
-          		<div style={{paddingTop: '14px', paddingLeft: '58px', fontFamily: 'Arial', fontWeight: 'bold'}}>Student McClass</div>
+          		<div style={{paddingTop: '14px', paddingLeft: '58px', fontFamily: 'Arial', fontWeight: 'bold'}}>{props.profile_name}</div>
           		<div style={{float: 'left', paddingLeft: '58px', fontFamily: 'Arial', fontWeight: 'bold', fontSize: '12', color: '#5A5A5A'}}>Student</div>
         	</div>  
       	</div>
