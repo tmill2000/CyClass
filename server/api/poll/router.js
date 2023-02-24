1ba221fef3809431259bd1dbc1253f9a04266b20
twmiller <twmiller@iastate.edu>
2023-02-23 18:06:32 -0600

    poll-course-apis

--- server/api/poll/router.js
+++ server/api/poll/router.js
@@ -3,11 +3,13 @@ const getPollResponse = require('./getPollResponse');
 const addPoll = require('./addPoll');
 const getPollMetrics = require('./getPollMetrics')
 const closePoll = require('./closePoll')
+const getPollById = require('./getPolls')
 
 module.exports = {
     ...addPollResponse,
     ...getPollResponse,
     ...addPoll,
     ...getPollMetrics,
-    ...closePoll
+    ...closePoll,
+    ...getPollById
 }
