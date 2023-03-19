ea25914fd66df4446027695b84c1ae74cec3934b
twmiller <twmiller@iastate.edu>
2023-03-19 18:48:38 -0500

    if user and new role are same adds to session

--- server/api/role/addRole.js
+++ server/api/role/addRole.js
@@ -19,6 +19,16 @@ const addRole = async (req, res) => {
             return res.status(401).send({ msg: "Unauthorized: Cannot add role." });
         }
         const insertId = await roleService.addRole(courseId, userId, role);
+        const course = await courseService.getCourse(courseId);
+        // + converts both variables to numbers
+        if (+userId === +req.session.userid) {
+            req.session.user_roles.push({
+                role_id: insertId,
+                course_id: courseId,
+                course_name: course[0].course_name,
+                role: role
+            });
+        }
         return res.status(201).send({ rollID: insertId });
     } catch (e) {
         console.error(e);
@@ -38,7 +48,7 @@ const addRoleByJoinCode = async (req, res) => {
             return res.status(400).send("Invalid Parameters");
         }
         const course = await courseService.getCourseByJoinCode(joinCode);
-        if (!!course?.closed) {
+        if (course?.closed) {
             return res.status(404).send({ msg: "Course Missing or Closed" });
         }
         const insertId = await roleService.addRole(course.course_id, req.session.userid, "STUDENT");
