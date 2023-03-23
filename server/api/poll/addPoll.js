4cb460409c567b28298131e97fa8abbd6cb3af7f
twmiller <twmiller@iastate.edu>
2023-03-23 17:31:43 -0500

    added poll type

--- server/api/poll/addPoll.js
+++ server/api/poll/addPoll.js
@@ -2,8 +2,9 @@ const pollService = require("./services/pollService");
 const { hasCoursePermissions } = require("../../utils/permissions");
 /**
  *
- * @param {*} req
- * @param {*} res
+ * @param {Express.Request} req
+ * @param {Express.Response} res
+ * @returns {Promise<Express.Response>}
  */
 const addPoll = async (req, res) => {
     try {
@@ -12,16 +13,17 @@ const addPoll = async (req, res) => {
             question_text: questionText,
             poll_choices: pollChoices,
             course_id: courseId,
-            close_date: closeDate
+            close_date: closeDate,
+            poll_type: pollType
         } = req.body;
         const senderId = req.session.userid;
-        if (!senderId || !lectureId || !questionText || !pollChoices || !courseId) {
+        if (!senderId || !lectureId || !questionText || !pollChoices || !courseId || !pollType) {
             return res.status(400).send({ msg: "Invalid Body" });
         }
         if (!hasCoursePermissions(courseId, req.session)) {
             return res.status(401).send({ msg: "Unauthorized: Cannot create poll" });
         }
-        const resp = await pollService.addPoll(senderId, lectureId, questionText, pollChoices, closeDate);
+        const resp = await pollService.addPoll(senderId, lectureId, questionText, pollChoices, pollType, closeDate);
         return res.status(201).send(resp);
     } catch (e) {
         console.error(e);
