b5839bac23e5bc89af285936b7fe2376ba6c741b
glin19 <glin19@iastate.edu>
2023-03-23 20:50:51 -0500

    made edits to patch service test

--- server/api/utils/patchService.test.js
+++ server/api/utils/patchService.test.js
@@ -7,18 +7,33 @@ describe("patchService", () => {
 
     describe("genericPatch", () => {
 
-        it("Return with 200", async () => {
-            jest.spyOn(db, "runQuery").mockRejectedValueOnce([
+        it("should return response", async () => {
+            jest.spyOn(db, "runQuery").mockResolvedValueOnce([
                 {
-                    
+                    message_id: 1,
+                    parent_id: null,
+                    sender_id: 1,
+                    lecture_id: 1,
+                    timestamp: "2023-3-28 17:41:28",
+                    body: "body"
                 }
-            ])
-            const res = await genericPatch()
-            expect(res).toBe()
+            ]);
+            const res = await genericPatch(null, null, null, 'new body');
+            expect(res).toEqual([
+                {
+                    message_id: 1,
+                    parent_id: null,
+                    sender_id: 1,
+                    lecture_id: 1,
+                    timestamp: "2023-3-28 17:41:28",
+                    body: "new body"
+                }
+            ]);
         });
+
         it("should throw error", async () => {
             jest.spyOn(db, "runQuery").mockRejectedValueOnce(new Error());
-            await expect(genericPatch(-1, null)).rejects.toThrow();
+            await expect(genericPatch(null, null, null, null)).rejects.toThrow();
         });
     })
 });
