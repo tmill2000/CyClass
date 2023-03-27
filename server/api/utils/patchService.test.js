d28c646c3580672b9005ad8a6fab50fabb9bf7db
glin19 <glin19@iastate.edu>
2023-03-27 01:10:37 -0500

    final edits to patchService.test

--- server/api/utils/patchService.test.js
+++ server/api/utils/patchService.test.js
@@ -3,19 +3,32 @@ const db = require("../../utils/db_connection");
 
 jest.mock("../../utils/db_connection");
 
+const tableKeyVal = {
+    courses: [{
+        course_id: 1
+    }],
+    messages: [{
+        message_title: "test",
+        is_anonymous: false,
+        body: 'body'
+    }]
+};
+
 describe("patchService", () => {
 
     describe("genericPatch", () => {
 
-        it("should return response", async () => {
+        it("should return", async () => {
             jest.spyOn(db, "runQuery").mockResolvedValueOnce([]);
             // const res = await genericPatch(tableName, newValsObj, whereClauseCol, whereClauseVal);
-            const res = await genericPatch(messages, messages.body, body, 'new body');
-            expect(res).toEqual([
-                {
-                    body: "new body"
-                }
-            ]);
+            const res = await genericPatch('courses', tableKeyVal, 'course_id', 2);
+            expect(res).toEqual([]);
+        });
+        
+        it("should return", async () => {
+            jest.spyOn(db, "runQuery").mockResolvedValueOnce([]);
+            const res = await genericPatch('messages', tableKeyVal, 'body', 'new body');
+            expect(res).toEqual([]);
         });
 
         it("should throw error", async () => {
