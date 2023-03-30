8447a3f36af82382782f96b2cc8d633c82be8fa5
JadenC1126 <jadenc@iastate.edu>
2023-03-30 15:48:27 -0500

    react forward ref can suck it

--- frontend/src/routes/HomePage/CourseRegistrationPopUp.jsx
+++ frontend/src/routes/HomePage/CourseRegistrationPopUp.jsx
@@ -7,59 +7,64 @@ import CourseComponent from "./CourseComponent";
 import './CourseRegistrationPopup.css';
 import DataStore from '../../utilities/data/DataStore';
 import CourseAPI from '../../utilities/api/CourseAPI';
-
-
-export default function CourseRegistrationPopup(props){
     
-    const courseAPI = new CourseAPI();
-    const input_url = createRef();
+const courseAPI = new CourseAPI();
+// const input_url = createRef();
 
-    this.state = {
-        rollID: '',
-        url: ''
+class CourseRegistrationPopUp extends React.Component {
+
+    constructor(props) {
+        this.state = {
+            rollID: '',
+            url: ''
+        }
     }
 
+
     getURL = async () => {
-        const typed_url = input_url.current.value.trim();
+        const typed_url = "";
         if (typed_url == "") {
             console.log("URL is empty.")
             return;
         }
         else{
-            const res_addCourse = await courseAPI.addCourseByUrl(this.state.url);
+            const res_addCourse = await courseAPI.addCourseByUrl(state.url);
             if (res_addCourse.accepted){
                 DataStore.set("rollID", res_addCourse.rollID);
             }
         }
     }
 
-    return (
-        <Popup 
-            trigger={<CourseComponent  color="gray" courseTitle="+"></CourseComponent>}
-            modal
-            nested
-        >
-            {close => (
-            <div className="course-reg-popup">
-                <button className="close" onClick={close}>&times;</button>
-                <div className="header"> Add A Course: </div>
-                <div className="course-reg-input-group">
-                    <div className="course-reg-input-label"></div>
-                    <input ref={input_url} className="course-reg-input-field" id="crURL"></input>
-                </div>
+    render() {
+        return (
+            <Popup 
+                trigger={<CourseComponent  color="gray" courseTitle="+"></CourseComponent>}
+                modal
+                nested
+            >
+                {close => (
+                <div className="course-reg-popup">
+                    <button className="close" onClick={close}>&times;</button>
+                    <div className="header"> Add A Course: </div>
+                    <div className="course-reg-input-group">
+                        <div className="course-reg-input-label"></div>
+                        <input  className="course-reg-input-field" id="crURL"></input>
+                    </div>
 
-                <div className="actions">
-                <button className="button"
-                    onClick={() => {
-                        getURL();
-                    }}>Add Course</button>
+                    <div className="actions">
+                    <button className="button"
+                        onClick={() => {
+                            getURL();
+                        }}>Add Course</button>
+                    </div>
                 </div>
-            </div>
-            )}   
-        </Popup>
-    );
+                )}   
+            </Popup>
+        );
+    }
 }
 
+export default CourseRegistrationPopUp;
 
 const ColoredLine = ({ color, height }) => (
   <hr
--- frontend/src/routes/HomePage/index.jsx
+++ frontend/src/routes/HomePage/index.jsx
@@ -1,49 +1,73 @@
 import React from "react";
+import Popup from 'reactjs-popup';
 import { Navigate } from "react-router-dom";
 import CourseComponent from "../HomePage/CourseComponent";
-// import { useParams } from "react-router-dom";
-import DataStore from '../../utilities/data/DataStore';
 import ErrorPage from "../ErrorPage";
+import LocalUser from "../../utilities/model/LocalUser";
+import CourseAPI from '../../utilities/api/CourseAPI';
 
 import './style.css';
-import CourseRegistrationPopup from "./CourseRegistrationPopUp";
+const courseAPI = new CourseAPI();
+import './CourseRegistrationPopup.css';
+
 
 function HomePage(props) {
-	const colors = ["red", "blue", "green", "brown"]
 
+	const state = {
+		rollID: '',
+		url: ''
+	}
+
+	const colors = ["red", "blue", "green", "brown"]
+	
 	const courses = structuredClone(LocalUser.useValue("courses"));
 	// Dropdown list generation
 	const dropdownList = [];
 	for (let i = 0; i < courses.length; i++) {
 		dropdownList.push(<CourseComponent courseTitle={courses[i]} color={colors[i]} />);
 	}
-	// Get lecture and course IDs from path params and validate them
-	// const pathParams = useParams();
-	// const lectureID = parseInt(pathParams.lectureID);
-	// if (isNaN(lectureID)) {
-	// 	return <ErrorPage code={400} text="Invalid lecture number" />;
-	// }
-	// const courseID = parseInt(pathParams.courseID);
-	// if (isNaN(courseID)) {
-	// 	return <ErrorPage code={400} text="Invalid course number" />;
-	// }
 
-	// Get user ID and permission level, verifying logged-in
-	// const userID = DataStore.get("userID");
-	// if (userID == null) {
-	// 	return <Navigate to="/login" />;
-	// }
-	// let isElevatedUser = false;
-	// for (const course of JSON.parse(DataStore.get("courses") || "[]")) {
-	// 	if (course.id == courseID) {
-	// 		isElevatedUser = course.role == "PROFESSOR" || course.role == "TA"
-	// 		break;
-	// 	}
-	// }
+
+    const getURL = async () => {
+		const url = document.getElementById("input_url");
+        const typed_url = url.current.value.trim();
+        if (typed_url == "") {
+            console.log("URL is empty.")
+            return;
+        }
+        else{
+            const res_addCourse = await courseAPI.addCourseByUrl(state.url);
+            if (res_addCourse.accepted){
+                LocalUser.set("rollID", res_addCourse.rollID);
+            }
+        }
+    }
 
 	return (
 		<div className="home-container">
-			<CourseRegistrationPopup></CourseRegistrationPopup>
+			<Popup 
+                trigger={<CourseComponent  color="gray" courseTitle="+"></CourseComponent>}
+                modal
+                nested
+            >
+                {close => (
+                <div className="course-reg-popup">
+                    <button className="close" onClick={close}>&times;</button>
+                    <div className="header"> Add A Course: </div>
+                    <div className="course-reg-input-group">
+                        <div className="course-reg-input-label"></div>
+                        <input id="input_url" className="course-reg-input-field"></input>
+                    </div>
+
+                    <div className="actions">
+                    <button className="button"
+                        onClick={() => {
+                            getURL();
+                        }}>Add Course</button>
+                    </div>
+                </div>
+                )}   
+            </Popup>
 			{dropdownList}
 		</div>
 	);
