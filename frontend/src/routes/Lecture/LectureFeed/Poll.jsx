b7b2de819f422334325ea2ba2f8b245618c582aa
bburt8701 <bburt8701@gmail.com>
2023-04-07 10:17:01 -0500

    Implemented API's to Poll.jsx (editing) with perms

--- frontend/src/routes/Lecture/LectureFeed/Poll.jsx
+++ frontend/src/routes/Lecture/LectureFeed/Poll.jsx
@@ -1,98 +1,146 @@
-/**
- * AUTHOR:	Adam Walters
- * CREATED:	02/05/2023
- * UPDATED:	02/21/2023
- */
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ProfileIcon from "../../../components/ProfileIcon";
+import TimeLabel from "./TimeLabel";
+import PollOption from "./PollOption";
+import "./styles.css";
+import LiveLectureAPI from "../../../api/LiveLectureAPI";
 
- import React, { useEffect, useState } from "react";
- import { Link } from "react-router-dom";
- 
- import ProfileIcon from "../../../components/ProfileIcon";
- 
- import TimeLabel from "./TimeLabel";
- import PollOption from "./PollOption";
- 
- import "./styles.css";
- 
- let allowResponse = true;
- const selectedCache = {};
- 
- function Poll(props) {
-	 // Define selected state
-	 const [selected, setSelected] = useState(selectedCache[props.id] || null);
-	 selectedCache[props.id] = selected
- 
-	 // Define interaction functions
-	 const onSelect = (choiceID) => {
-		 // If open and not already responding, send response
-		 if (!props.closed && props.api != null && allowResponse) {
-			 allowResponse = false;
-			 props.api.respondToPoll(props.id, choiceID)
-				 .then(() => setSelected(choiceID))
-				 .finally(() => allowResponse = true);
-		 }
-	 };
-	 const onClose = () => {
-		 // If open, then close the poll
-		 if (!props.closed && props.api != null) {
-			 props.api.closePoll(props.id);
-		 }
-	 };
- 
-	 // Lazy-retrieve user's selection
-	 useEffect(() => {
-		 if (props.api != null && props.id != null && selected == null) {
-			 props.api.getPollResponse(props.id)
-				 .then((choice) => {
-					 if (choice.choiceID != null) {
-						 setSelected(choice.choiceID);
-					 }
-				 });
-		 }
-	 }, [ props.id, props.closed ]);
- 
-	 // Sort choices by ID
-	 const choices = (props.choices || {}).map(x => x).sort((x, y) => x.id - y.id);
- 
-	 // Make URL for results
-	 const resultsURL = `results?poll=${props.id}`;
- 
-	 // Component
-	 return (
-		 <li className={`poll ${props.me ? "me" : ""}`}>
-			 <div>
-				 <TimeLabel time={props.time}/>
-				 <div className="post-bubble">
-					 <div className="poll-header">
-						 <div className="title-container">
-							 <div className="container">
-								 <span className="title">POLL{props.closed ? " (closed)" : ""}</span>
-							 </div>
-							 <div className="line" />
-						 </div>
-						 {!props.closed && props.elevated ? <button className="button close" onClick={onClose}>CLOSE</button> : null}
-						 {props.elevated ? <Link className="button results" to={resultsURL}>VIEW PARTICIPATION</Link> : null}
-					 </div>
-					 <div className="content">
-						 <p>{props.prompt}</p>
-						 <div>
-							 {choices.map(x => <PollOption key={x.id.toString()}
-								 onSelect={!props.closed ? () => onSelect(x.id) : null}
-								 selected={x.id == selected}
-								 correct={props.closed ? x.correct == true : null}>
-								 {x.text}
-							 </PollOption>)}
-						 </div>
-					 </div>
-				 </div>
-			 </div>
-			 {(props.user != null) ?
-			 <div className="user-container">
-				 <ProfileIcon profile_name={props.me ? "You" : props.user.name} profile_role={props.user.role} flipped={true} />
-			 </div>
-			 : null}
-		 </li>
-	 );
- }
- 
- export default Poll; 
+let allowResponse = true;
+const selectedCache = {};
+
+function Poll(props) {
+  const [selected, setSelected] = useState(selectedCache[props.id] || null);
+  selectedCache[props.id] = selected;
+
+  const [pollPrompt, setPollPrompt] = useState(props.prompt);
+  const [pollChoices, setPollChoices] = useState(props.choices || []);
+  const [hovering, setHovering] = useState(false); // New state hook for hover tracking
+
+  useEffect(
+    () => {
+      if (props.api != null && props.id != null && selected == null) {
+        props.api.getPollResponse(props.id).then((choice) => {
+          if (choice.choiceID != null) {
+            setSelected(choice.choiceID);
+          }
+        });
+      }
+    },
+    [props.api, props.id, selected]
+  );
+
+  const canEditPoll = props.user.id === props.creator.id;
+
+  const editPollPrompt = () => {
+    const updatedPrompt = prompt("Enter updated poll prompt:");
+    if (updatedPrompt != null) {
+      LiveLectureAPI.editPollPrompt(props.id, updatedPrompt).then(() => {
+        setPollPrompt(updatedPrompt);
+      });
+    }
+  };
+
+  const editPollChoice = (choiceId) => {
+    const updatedChoice = prompt("Enter updated poll choice:");
+    if (updatedChoice != null) {
+      LiveLectureAPI.editPollChoice(choiceId, updatedChoice).then(() => {
+        const newChoices = pollChoices.map((choice) => {
+          if (choice.id === choiceId) {
+            return { ...choice, text: updatedChoice };
+          } else {
+            return choice;
+          }
+        });
+        setPollChoices(newChoices);
+      });
+    }
+  };
+
+  const editPollResponse = (responseId) => {
+    const updatedResponse = prompt("Enter updated poll response:");
+    if (updatedResponse != null) {
+      LiveLectureAPI.editPollResponse(responseId, updatedResponse).then(() => {
+        setSelected(responseId);
+      });
+    }
+  };
+
+  const onSelect = (choiceID) => {
+    if (!props.closed && props.api != null && allowResponse) {
+      allowResponse = false;
+      props.api
+        .respondToPoll(props.id, choiceID)
+        .then(() => setSelected(choiceID))
+        .finally(() => (allowResponse = true));
+    }
+  };
+
+  const onClose = () => {
+    if (!props.closed && props.api != null) {
+      props.api.closePoll(props.id);
+    }
+  };
+
+  const choices = pollChoices.sort((x, y) => x.id - y.id);
+
+  const resultsURL = results ? `poll=${props.id}` : "";
+
+  const showEditButtons = hovering && props.elevated && canEditPoll && !props.closed;
+
+  return (
+    <li
+className={`poll ${props.me ? "me" : ""}`}
+onMouseEnter={() => setHovering(true)}
+onMouseLeave={() => setHovering(false)}
+>
+<div>
+   <TimeLabel time={props.time} />
+   <div className="post-bubble">
+      <div className="poll-header">
+         <h3>{pollPrompt}</h3>
+         {props.creator && (
+         <div className="poll-author">
+            <ProfileIcon user={props.creator} />
+            <span>{props.creator.username}</span>
+         </div>
+         )}
+      </div>
+      <div className="poll-choices">
+         {choices.map((choice) => (
+         <PollOption
+         key={choice.id}
+         choice={choice}
+         selected={selected === choice.id}
+         onSelect={onSelect}
+         canEdit={showEditButtons}
+         onEdit={() => editPollChoice(choice.id)}
+         />
+         ))}
+      </div>
+      <div className="poll-footer">
+         {props.me && (
+         <div className="poll-buttons">
+            {canEditPoll && (
+            <button className="edit-button" onClick={editPollPrompt}>
+            Edit Prompt
+            </button>
+            )}
+            {!props.closed && (
+            <button className="close-button" onClick={onClose}>
+            Close Poll
+            </button>
+            )}
+         </div>
+         )}
+         <div className="poll-result-link">
+            <Link className="button results" to={resultsURL}>
+            VIEW PARTICIPATION</Link>
+         </div>
+      </div>
+   </div>
+</div>
+</li>
+);
+}
+export default Poll;
